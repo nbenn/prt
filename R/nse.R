44938daff1d94508b4a0c4604412425e54d4ee8a
Nicolas Bennett <nicolas.bennett@stat.math.ethz.ch>
2020-05-16 13:27:45 +0200

    fix subset() for curly-curly args

--- R/nse.R
+++ R/nse.R
@@ -23,8 +23,16 @@ subset.prt <- function(x, subset, select, part_safe = FALSE, drop = FALSE,
   if (missing(subset)) subset <- NULL
   else subset <- rlang::enquo(subset)
 
+  if (rlang::is_quosure(subset) && rlang::quo_is_missing(subset)) {
+    subset <- NULL
+  }
+
   if (missing(select)) select <- NULL
   else select <- rlang::enquo(select)
+
+  if (rlang::is_quosure(select) && rlang::quo_is_missing(select)) {
+    select <- NULL
+  }
 
   subset_quo(x, subset, select, part_safe)
 }
