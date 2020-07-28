c8dac56c3dae1187d3c4c52717488963af2372c0
Nicolas Bennett <nicolas.bennett@stat.math.ethz.ch>
2020-07-28 11:48:37 +0200

    add covrignore

--- R/zzz.R
+++ R/zzz.R
@@ -2,6 +2,6 @@
 #' @importFrom assertthat assert_that is.count is.string is.flag
 NULL
 
-.onLoad <- function(libname, pkgname) { # nocov start
+.onLoad <- function(libname, pkgname) {
   backports::import(pkgname, c("...length", "isFALSE", "strrep", "trimws"))
-} # nocov end
+}
